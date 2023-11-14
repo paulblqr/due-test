093607ae63cebbd90a40636c119a7298848cf487
paulblqr <paul@mkdx.io>
2023-11-14 10:39:12 +0100

    add svgs, theme, mixins

--- src/app/components/Banner/Banner.jsx
+++ src/app/components/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import style from "./Banner.module.scss";
 export default function Banner() {
-  return <div>Banner</div>;
+  return <div className={style.Container}>My Diduenjoy Test case</div>;
 }
